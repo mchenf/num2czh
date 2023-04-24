ab38fbd2689e54300c3430410902b570c19c0879
mchenf <mochen@foss.dk>
2023-04-24 13:25:57 +0800

    Completed decimal points process

--- n2czh/Program.cs
+++ n2czh/Program.cs
@@ -19,9 +19,10 @@ namespace n2czh
             (char)25342, (char)20336, (char)20191,
             (char)19975, (char)20159, (char)20806
         };
+        //圆角分整
         readonly static char[] lsCurrency = new char[]
         {
-            '\u5706', '\u89d2', '\u5206'
+            '\u5706', '\u89d2', '\u5206', '\u6574'
         };
 
         readonly static string rxNumber = @"^\d+(\.\d{1,2}){0,1}$";
@@ -55,24 +56,44 @@ namespace n2czh
                 );
                 return;
             }
-            //处理小数点之后的
+
+            // 参数拆分为小数点之前与之后
             string[] NumParts = args[0].Split('.');
+            bool needZheng = true;
 
             var resultSB = new StringBuilder();
-            if(NumParts.Length == 2)
+
+            // 处理小数点之后的
+            if (NumParts.Length == 2)
             {
                 // len 不会超过 2， 因为 rxNumber
                 int len = NumParts[1].Length;
                 for (int i = 0; i < len; i++)
                 {
+                    string digit = NumParts[1].Substring(i, 1);
+                    if (digit == "0") continue; //小数点后面的不需要输出零
                     int numIndex = int.Parse(NumParts[1].Substring(i, 1));
                     resultSB.Append(lsChars[numIndex])
                             .Append(lsCurrency[1 + i]);
+                    if (needZheng) needZheng = false;
                 }
             }
 
+            // 处理小数点之前的部分
+
+
+
+            if (needZheng)
+            {
+                resultSB.Append(lsCurrency[3]);
+            }
             Console.WriteLine(resultSB.ToString());
 
+
+        }
+
+        internal static void ConvertN2czh(int unitDigit, int unitLength, StringBuilder strBuilder)
+        {
 
         }
     }
