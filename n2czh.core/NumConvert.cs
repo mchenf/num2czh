fb3ee9f0b4dd1545c83607dd808062955d0cf1cb
mchenf <mochen@foss.dk>
2023-04-27 14:52:07 +0800

    Standardized NumConvert Class with comments, modified tests

--- n2czh.core/NumConvert.cs
+++ n2czh.core/NumConvert.cs
@@ -3,15 +3,26 @@ using System.Text.RegularExpressions;
 
 namespace n2czh.core
 {
-    
+    /// <summary>
+    /// 代表一个处理从阿拉伯数字到中文大写金额的任务
+    /// </summary>
     public class NumConvert
     {
         private static readonly string validNumberString = @"^[1-9]\d+(\.\d{1,2}){0,1}$";
         private int[] numbers = new int[32];
+        /// <summary>
+        /// 当前整数部分的长度
+        /// </summary>
         public int Length { get; private set; } = 0;
-        public bool HasDecimal() => decimals[0] > 0 && decimals[1] > 0;
+        /// <summary>
+        /// 当前小数部分的长度
+        /// </summary>
+        public int LengthDecimal { get; private set; } = 0;
         private int[] decimals = new int[2];
-
+        /// <summary>
+        /// 代表一个处理从阿拉伯数字到中文大写金额的任务
+        /// </summary>
+        /// <param name="input">输入的代表数字的字符串</param>
         public NumConvert(string input)
         {
             if (Validate(input))
@@ -26,21 +37,33 @@ namespace n2czh.core
                 i++;
                 for (int j = 0; i < len; i++, j++) {
                     decimals[j] = input[i] - '0';
+                    LengthDecimal++;
                 }
             }
         }
-
+        /// <summary>
+        /// 获得数字字符串的某一位
+        /// </summary>
+        /// <param name="index">位数数字的序数</param>
+        /// <param name="NumOn">当为 true 时，返回整数部分； false 时返回小数部分</param>
+        /// <returns>位数上所代表的数字</returns>
+        /// <exception cref="IndexOutOfRangeException">
+        /// </exception>
         public int this[int index, bool NumOn]
         {
             get 
             {
-                int limit = NumOn ? Length : 1;
+                int limit = NumOn ? Length : LengthDecimal;
                 if (index > limit) throw new IndexOutOfRangeException();
 
                 return NumOn ? numbers[index] : decimals[index];
             }
         }
-
+        /// <summary>
+        /// 检查某字符串是否符合 #00(.00) 的标准
+        /// </summary>
+        /// <param name="number">待检字符串</param>
+        /// <returns>true 符合标准，false 不符合</returns>
         public static bool Validate(string number)
         {
             return Regex.IsMatch(number, validNumberString);
--- n2czh.coreTests/NumConvertTests.cs
+++ n2czh.coreTests/NumConvertTests.cs
@@ -32,15 +32,16 @@ namespace n2czh.core.Tests
         {
 
         }
-        [DataRow("332774", new int[] { 0, 0, 3, 3, 2, 7, 7, 4 }, DisplayName = "Short Digit Only Number")]
-        [DataRow("2217.73", new int[] { 7, 3, 2, 2, 1, 7 }, DisplayName = "Short Digit Number, Decimal")]
-        [DataRow("223359988000332", new int[] { 0, 0, 2, 2, 3, 3, 5, 9, 9, 8, 8, 0, 0, 0, 3, 3, 2 }, DisplayName = "Long Digit Only Number")]
-        [DataRow("223359988000332.89", new int[] { 8, 9, 2, 2, 3, 3, 5, 9, 9, 8, 8, 0, 0, 0, 3, 3, 2 }, DisplayName = "Long Digit Number, Decimal")]
+
         [TestMethod()]
-        public void NumConvertTest(string NumberString, int[] expectedArgs)
+        [DataRow("332774", new int[] {}, new int[] {3, 3, 2, 7, 7, 4 }, DisplayName = "Short Digit Only Number")]
+        [DataRow("2217.73", new int[] { 7, 3 }, new int[] { 2, 2, 1, 7 }, DisplayName = "Short Digit Number, Decimal")]
+        [DataRow("223359988000332", new int[] {}, new int[] { 2, 2, 3, 3, 5, 9, 9, 8, 8, 0, 0, 0, 3, 3, 2 }, DisplayName = "Long Digit Only Number")]
+        [DataRow("223359988000332.89", new int[] { 8, 9 }, new int[] { 2, 2, 3, 3, 5, 9, 9, 8, 8, 0, 0, 0, 3, 3, 2 }, DisplayName = "Long Digit Number, Decimal")]
+        public void NumConvertTest(string NumberString, int[] expectedDecimal, int[] expectedNumber)
         {
             var actual = new NumConvert(NumberString);
-            ExpectedNumConvert expected = new ExpectedNumConvert(expectedArgs);
+            ExpectedNumConvert expected = new ExpectedNumConvert(expectedDecimal, expectedNumber);
             Assert.IsTrue(expected == actual);
 
         }
@@ -48,16 +49,20 @@ namespace n2czh.core.Tests
 
     public struct ExpectedNumConvert
     {
-        public int Length { get; private set; }
+        public int Length { get; private set; } = 0;
+        public int LengthDecimal { get; private set; } = 0;
         public int[] Numbers { get; set; } = new int[32];
         public int[] Decimals { get; set; } = new int[2];
-        public ExpectedNumConvert(int[] args)
+        public ExpectedNumConvert(int[] decs, int[] nums)
         {
-            (Decimals[0], Decimals[1]) = (args[0], args[1]);
-            Length = 0;
-            for (int i = 0; i < Math.Min(args.Length - 2, 32); i++)
+            for (int i = 0; i < decs.Length; i++)
             {
-                Numbers[i] = args[i + 2];
+                Decimals[i] = decs[i];
+                LengthDecimal++;
+            }
+            for (int i = 0; i < Math.Min(nums.Length, 32); i++)
+            {
+                Numbers[i] = nums[i];
                 Length++;
             }
         }
@@ -66,11 +71,12 @@ namespace n2czh.core.Tests
         {
 
             if (left.Length != right.Length) return false;
+            if (left.LengthDecimal != right.LengthDecimal) return false;
             for (int i = 0; i < left.Length; i++)
             {
                 if (left.Numbers[i] != right[i, true]) return false;
             }
-            for (int i = 0; i < 2; i++)
+            for (int i = 0; i < left.LengthDecimal; i++)
             {
                 if (left.Decimals[i] != right[i, false]) return false;
             }
