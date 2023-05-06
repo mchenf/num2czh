d4dd425bc42b8da42d11ce9d544e897c4bbe2e2f
mchenf <mochen@foss.dk>
2023-05-06 15:56:13 +0800

    Removed NumConverter test, because type changed from int[] to char[]

--- n2czh.coreTests/NumConvertTests.cs
+++ n2czh.coreTests/NumConvertTests.cs
@@ -35,19 +35,6 @@ namespace n2czh.core.Tests
         }
 
         [TestMethod()]
-        [DataRow("332774", new int[] { }, new int[] { 3, 3, 2, 7, 7, 4 }, DisplayName = "Short Digit Only Number")]
-        [DataRow("2217.73", new int[] { 7, 3 }, new int[] { 2, 2, 1, 7 }, DisplayName = "Short Digit Number, Decimal")]
-        [DataRow("223359988000332", new int[] { }, new int[] { 2, 2, 3, 3, 5, 9, 9, 8, 8, 0, 0, 0, 3, 3, 2 }, DisplayName = "Long Digit Only Number")]
-        [DataRow("223359988000332.89", new int[] { 8, 9 }, new int[] { 2, 2, 3, 3, 5, 9, 9, 8, 8, 0, 0, 0, 3, 3, 2 }, DisplayName = "Long Digit Number, Decimal")]
-        public void NumConvertTest(string NumberString, int[] expectedDecimal, int[] expectedNumber)
-        {
-            var actual = new NumConvert(NumberString);
-            ExpectedNumConvert expected = new ExpectedNumConvert(expectedDecimal, expectedNumber);
-            Assert.IsTrue(expected == actual);
-
-        }
-
-        [TestMethod()]
         [DataRow("332", "", "332")]
         [DataRow("3322225567", "332222", "5567")]
         [DataRow("3311122", "331", "1122")]
@@ -105,60 +92,6 @@ namespace n2czh.core.Tests
             var n = new NumConvert(testee);
             string actual = n.ToString();
             Assert.AreEqual(expected, actual);
-        }
-    }
-
-    public struct ExpectedNumConvert
-    {
-        public int Length { get; private set; } = 0;
-        public int LengthDecimal { get; private set; } = 0;
-        public int[] Numbers { get; set; } = new int[32];
-        public int[] Decimals { get; set; } = new int[2];
-        public ExpectedNumConvert(int[] decs, int[] nums)
-        {
-            for (int i = 0; i < decs.Length; i++)
-            {
-                Decimals[i] = decs[i];
-                LengthDecimal++;
-            }
-            for (int i = 0; i < Math.Min(nums.Length, 32); i++)
-            {
-                Numbers[i] = nums[i];
-                Length++;
-            }
-        }
-
-        public static bool operator ==(ExpectedNumConvert left, NumConvert right)
-        {
-
-            if (left.Length != right.Length) return false;
-            if (left.LengthDecimal != right.LengthDecimal) return false;
-            for (int i = 0; i < left.Length; i++)
-            {
-                if (left.Numbers[i] != right[i, true]) return false;
-            }
-            for (int i = 0; i < left.LengthDecimal; i++)
-            {
-                if (left.Decimals[i] != right[i, false]) return false;
-            }
-
-            return true;
-        }
-
-        public static bool operator !=(ExpectedNumConvert left, NumConvert right)
-        {
-            return !(left == right);
-
-            
-        }
-
-        public override bool Equals(object? obj)
-        {
-            return base.Equals(obj);
-        }
-        public override int GetHashCode()
-        {
-            return base.GetHashCode();
         }
     }
 }
